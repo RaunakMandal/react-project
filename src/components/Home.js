7a219414ccf56054eb4d56ed5bad14af60feb2eb
Raunak Mandal <45995056+RaunakMandal@users.noreply.github.com>
2022-10-29 23:20:51 +0530

    Add Form and Profile Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,25 @@
-import React, { Component } from "react";
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Add your react Components.</h1>
-            </div>
-        );
-    }
-}
+import React, { useState } from "react";
+import Profile from "./Profile";
+const Home = () => {
+  const [username, setUsername] = useState("");
+  const handleNameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const fetchFromAPI = () => {
+    console.log(username);
+  };
+  return (
+    <div>
+      <h1>GitHub Profile Fetcher</h1>
+      <input
+        type="text"
+        placeholder="GitHub username"
+        onChange={(e) => handleNameChange(e)}
+      />
+      <button onClick={fetchFromAPI}>Fetch</button>
+      <Profile />
+    </div>
+  );
+};
 
 export default Home;
